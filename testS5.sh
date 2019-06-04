54eccaee94d672136edf30538cb162c14bdfad9b
STAGIAIRE Be Electronique <STBE@kgf.fr>
2019-06-04 16:05:31 +0200

    update: testS5 + del: dependencies content

--- testS5.sh
+++ testS5.sh
@@ -1,13 +1,22 @@
-repo_name=$(basename `git rev-parse --show-toplevel`);
-if grep -Fxq "$repo_name" dependencies
-then
-	echo "in dep";
+repo_name=$(basename `git rev-parse --show-toplevel`)
+arg=$0;
+filename=$(basename "$0")
+dep_path=${arg%"$filename"}
+dep_name="dependencies"
+dep_path="$dep_path$dep_name"
 
+IFS=' ' read -r branch urlplus <<< "$(git remote -v)"
+read -r fetch url <<< "$branch"
+
+if grep -Fxq "$repo_name" $dep_path
+then
+	echo "in dep"
 else
-	echo "not in dep";
-	echo $repo_name >> dependencies;
+	echo "not in dep - updating..."
+	echo "$repo_name $url" >> $dep_path
+	echo "added to dep"
 fi
 
 echo "";
-echo "dependencies :";
-cat dependencies;
+echo "dependencies :"
+cat $dep_path
